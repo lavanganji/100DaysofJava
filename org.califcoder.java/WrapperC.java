d743a54a741c6e76537dce44885a7a72bb5b45d2
Lavan Kumar <11154769+lavanganji@users.noreply.github.com>
2020-02-18 12:53:34 -0600

    Wrapper Class Example.
    Wrapper Class Example.

---
+++ org.califcoder.java/WrapperC.java
@@ -0,0 +1,14 @@
+package org.califcoder.java;
+
+public class WrapperC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b = Byte.MAX_VALUE;
+		System.out.println("The Max Value of b "+b);
+		b++;
+		System.out.println("The Value of b after incrementing is:"+b);
+		
+	}
+
+}
