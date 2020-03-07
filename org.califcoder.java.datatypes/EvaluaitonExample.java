dba61fae25f7163f21da69ff4f7787a3504ec32e
Lavan Kumar <11154769+lavanganji@users.noreply.github.com>
2020-03-07 17:51:47 -0600

    Create EvaluaitonExample.java

---
+++ org.califcoder.java.datatypes/EvaluaitonExample.java
@@ -0,0 +1,7 @@
+public class EvaluaitonExample {
+  public static void main(Strings args[]){
+    int a = 2, b = 3, c = 4;
+    int d  = a+b*c;
+    System.out.println("The Value of d after evaulating the expression is "+d);
+  }
+}
