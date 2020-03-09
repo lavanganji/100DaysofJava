0c4e90b59fbf1afbc66b2fb308a6e29121d78368
Lavan Kumar <11154769+lavanganji@users.noreply.github.com>
2020-03-09 11:31:14 -0500

    Create CC1_FirstJava.java

---
+++ org.califcoder.javabasics/CC1_FirstJava.java
@@ -0,0 +1,17 @@
+package org.califcoder.javabasics;
+
+/**
+ * @author Calif Coder
+ *
+ */
+public class CC1_FirstJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the First Java Program");
+	}
+
+}
