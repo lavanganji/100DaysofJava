e5fd810a0bcb15099740557c4aec030c8b2ab66e
lavanganji <lavanganji@gmail.com>
2021-06-20 23:40:29 -0500

    Changed Pkging Structure

---
+++ src/org/califcoder/javabasics/ClassAndObjects.java
@@ -0,0 +1,23 @@
+package org.califcoder.javabasics;
+
+public class ClassAndObjects {
+
+	public static void main(String[] args) {
+		
+		Welcome welcome = new Welcome();
+		System.out.println("Printing Message from Welcom Class");
+		welcome.sayHello();
+		System.out.println("Printing Message from Welcom Properties\n"+welcome.welcome);
+
+
+	}
+
+}
+
+ class Welcome{
+	public String welcome = "Hello Welcome to Java Basics Class and Objects!";
+	public void sayHello() {
+		System.out.println("Message is :"+welcome);
+		
+	}
+}
