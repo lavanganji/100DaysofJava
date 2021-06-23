68e6e6cc35d42af7f1e71026fa8fe49fb5e14cb3
lavanganji <lavanganji@gmail.com>
2021-06-23 09:06:09 -0500

    LOCAL VARIABLES

--- src/org/califcoder/javabasics/_1LocalVariables.java
+++ src/org/califcoder/javabasics/_1LocalVariables.java
@@ -8,20 +8,21 @@ public class _1LocalVariables {
 				+ "\nWe Cannot call the variables out the funcitons since their scope is limited to that function.");
 		getMessageFunction();
 		
-		//System.out.println("Pringing the Message from inside the funcion\t"+message);
+		//System.out.println("Printing the Message from inside the funcion\t"+message);
 		
-		System.out.println("The above line is giving the compile time error"
-				+ "message cannot be resolved to a variable"
-				+ "Since message is declared in the function getMessageFunction"
-				+ "message is local to getMessageFunction only."
-				+ "To overcome this , message should be declared in the class level.");
+		System.out.println("\n\nThe above line is giving the compile time error"
+				+ "\nmessage cannot be resolved to a variable"
+				+ "\nSince message is declared in the function getMessageFunction"
+				+ "\nmessage is local to getMessageFunction only."
+				+ "\nTo overcome this , message should be declared in the class level."
+				+ "\n\nThese are called FEILDS as known as Class Level Variables.");
 
 		
 	}
 
 	private static void getMessageFunction() {
 		String message = new String("This is the Message");
-		System.out.println("Pringing the Message from inside the funcion\t"+message);
+		System.out.println("\nPrinting the Message from inside the funcion :\t"+message);
 	}
 	
 	
